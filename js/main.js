a0cf6f481b5ce1c2ed80099a1aaa3184fb27c94f
RSmetanyuk <rsmetanyuk@gmail.com>
2017-07-13 15:07:58 +0300

    #52. Clear error messag on reset

--- js/main.js
+++ js/main.js
@@ -46,6 +46,7 @@ function resetData () {
   document.getElementById("startRow").disabled = true;
   document.getElementById("hide").style.display = "none";
   document.getElementById("returnToTopBottomBuckground").style.display = "none";
+  document.getElementById("errorMessage").style.visibility  = "hidden";
 };
 
 window.onscroll = function() {
