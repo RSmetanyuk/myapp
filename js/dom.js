539d3428dfce943b1d384502453d48b489b0b28b
RSmetanyuk <rsmetanyuk@gmail.com>
2017-08-19 15:33:27 +0300

    Moved code from main to dom

--- js/dom.js
+++ js/dom.js
@@ -67,15 +67,15 @@ var dom = {
                     "</td><td>" + person3.winsCounter + 
                     "</td><td>" + person4.winsCounter + "</td></tr></tbody>";
 
-    document.getElementById("generalInfo").innerHTML = 
+    document.getElementById("totalNumberOfWins").innerHTML = 
     "Total number of wins is " + dom.counterWinsInStrategy;
     
-    person1.winsCounter = 0;
+    /*person1.winsCounter = 0;
     person2.winsCounter = 0;
     person3.winsCounter = 0;
     person4.winsCounter = 0;
     dom.counterWinsInStrategy = 0;
-    dom.combinationCounter = 0;
+    dom.combinationCounter = 0;*/
   },
 
   pageNumber: function (n) { // "n" - number first row on page
@@ -231,5 +231,62 @@ var dom = {
       document.getElementById("returnToTopBottomBuckground").style.display = "none";
     };
   },
-  
+
+  onScrollHandler: function () {
+    if (document.body.offsetHeight > document.documentElement.clientHeight) {
+      document.getElementById("returnToTopBottomBuckground").style.display = "inline";
+      document.getElementById("returnToBottom").style.display = "inline";
+    } else {
+      document.getElementById("returnToBottom").style.display = "none"; 
+    };
+
+
+    if (window.pageYOffset >= 1) {
+      document.getElementById("returnToTop").style.display = "inline";     
+    } else {
+      document.getElementById("returnToTop").style.display = "none";
+    };
+
+    if (window.pageYOffset > document.body.offsetHeight - document.documentElement.clientHeight - 1) {
+      document.getElementById("returnToBottom").style.display = "none"; 
+    };
+  },
+
+  start: function () {
+    document.getElementById("Reset").style.display = "inline";
+    document.getElementById("Start").style.display = "none";
+    document.getElementById("totalRows").innerHTML = dom.combinationTxt.length;
+    document.getElementById("numberCombinationsInPage").disabled = false;
+    document.getElementById("numberCombinationsInPage").value = 5;
+    document.getElementById("numberCombinationsInPageOverlay").style.display = "none";
+    document.getElementById("startRow").disabled = false;
+    document.getElementById("startRowOverlay").style.display = "none";
+    document.getElementById("errorMessage").style.visibility  = "hidden";
+    document.getElementById("pagination").style.display = "inline-block";
+    dom.pageNumber(0);
+  },
+
+  reset: function () {
+    document.getElementById("Reset").style.display = "none";
+    document.getElementById("Start").style.display = "inline";
+    document.getElementById("tableBody").innerHTML = "";
+    /*document.getElementById("table2Body").innerHTML = "";
+    document.getElementById("table2v2").innerHTML =
+      "<tbody><tr><th>Persons</th><td>A</td><td>B</td><td>C</td><td>D</td></tr>" +
+      "<th>Wins count</th><td></td><td></td><td></td><td></td></tr></tbody>";*/   
+    document.getElementById("totalNumberOfWins").innerHTML = "";
+    document.getElementById("totalRows").innerHTML = 0;
+    document.getElementById("numberCombinationsInPage").value = "";
+    document.getElementById("numberCombinationsInPage").disabled = true;
+    document.getElementById("numberCombinationsInPageOverlay").style.display = "inline-block";
+    dom.rowInPage = 5;
+    document.getElementById("startRow").value = "";
+    document.getElementById("startRow").disabled = true;
+    document.getElementById("startRowOverlay").style.display = "inline-block";
+    dom.firstVisibleRow = 0;
+    document.getElementById("pagination").style.display = "none";
+    document.getElementById("returnToTopBottomBuckground").style.display = "none";
+    document.getElementById("errorMessage").style.visibility  = "hidden";
+  }
+
 };
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 function main () {
-  document.getElementById("Reset").style.display = "inline";
-  document.getElementById("Start").style.display = "none";
-  dom.combinationTxt = [];
-  
+
   var person1 = new Person1 (0);
   var person2 = new Person2 (0);
   var person3 = new Person3 (0);
@@ -23,64 +20,11 @@ function main () {
     };
   };
   dom.writeGeneralInfo(person1, person2, person3, person4);
-  document.getElementById("totalRows").innerHTML = dom.combinationTxt.length;
-  document.getElementById("numberCombinationsInPage").disabled = false;
-  document.getElementById("numberCombinationsInPage").value = 5;
-  document.getElementById("numberCombinationsInPageOverlay").style.display = "none";
-  document.getElementById("startRow").disabled = false;
-  document.getElementById("startRowOverlay").style.display = "none";
-  document.getElementById("errorMessage").style.visibility  = "hidden";
-  document.getElementById("pagination").style.display = "inline-block";
-  dom.pageNumber(0);
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-};
-
-function resetData () {
-  document.getElementById("Reset").style.display = "none";
-  document.getElementById("Start").style.display = "inline";
-  document.getElementById("tableBody").innerHTML = "";
-  document.getElementById("table2Body").innerHTML = "";
-  document.getElementById("table2v2").innerHTML =
-      "<tbody><tr><th>Persons</th><td>A</td><td>B</td><td>C</td><td>D</td></tr>" +
-                 "<th>Wins count</th><td></td><td></td><td></td><td></td></tr></tbody>";
-  document.getElementById("generalInfo").innerHTML = "";
-  document.getElementById("totalRows").innerHTML = 0;
-  document.getElementById("numberCombinationsInPage").value = "";
-  document.getElementById("numberCombinationsInPage").disabled = true;
-  document.getElementById("numberCombinationsInPageOverlay").style.display = "inline-block";
-  dom.rowInPage = 5;
-  document.getElementById("startRow").value = "";
-  document.getElementById("startRow").disabled = true;
-  document.getElementById("startRowOverlay").style.display = "inline-block";
-  dom.firstVisibleRow = 0;
-  document.getElementById("pagination").style.display = "none";
-  document.getElementById("returnToTopBottomBuckground").style.display = "none";
-  document.getElementById("errorMessage").style.visibility  = "hidden";
-};
-
-window.onscroll = function() {
-    if (document.body.offsetHeight > document.documentElement.clientHeight) {
-      document.getElementById("returnToTopBottomBuckground").style.display = "inline";
-      document.getElementById("returnToBottom").style.display = "inline";
-    } else {
-      document.getElementById("returnToBottom").style.display = "none"; 
-    };
-
-
-    if (window.pageYOffset >= 1) {
-      document.getElementById("returnToTop").style.display = "inline";     
-    } else {
-      document.getElementById("returnToTop").style.display = "none";
-    };
-
-    if (window.pageYOffset > document.body.offsetHeight - document.documentElement.clientHeight - 1) {
-      document.getElementById("returnToBottom").style.display = "none"; 
-    };
+  dom.start();
 };
 
 window.onload = function() {
   main();
 };
+
+window.onscroll = dom.onScrollHandler;
